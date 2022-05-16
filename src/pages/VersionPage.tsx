190ff8621cd2f8da1900fea8df9b89cfe58572cc
rmznkapar <rmznkapar@gmail.com>
2022-05-16 23:05:13 +0300

    orospu burak

--- src/pages/VersionPage.tsx
+++ src/pages/VersionPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VersionPage = () => {
   return (
-    <div className="text-center flex justify-center mt-16">
-      <div className="font-bold text-2xl">
+    <div className="text-center flex justify-center pt-16 w-screen h-screen bg-blue-600">
+      <div className="font-bold text-3xl text-white">
         <p>Version 1</p>
         <p>Kubernetes Test</p>
         <p>Pod Name: {import.meta.env.HOSTNAME}</p>
