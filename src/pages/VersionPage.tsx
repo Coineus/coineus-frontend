6912c30951b452ed26cb8a8b16cd5f569f0b9365
safderun <burakberkkeskin@gmail.com>
2022-05-28 14:52:36 +0300

    version update

--- src/pages/VersionPage.tsx
+++ src/pages/VersionPage.tsx
@@ -4,7 +4,7 @@ const VersionPage = () => {
   return (
     <div className="text-center flex justify-center pt-16 w-screen h-screen bg-blue-600">
       <div className="font-bold text-3xl text-white">
-        <p>Version 1</p>
+        <p>Version 2</p>
         <p>Kubernetes Test</p>
         <p>Pod Name: {import.meta.env.VITE_HOSTNAME}</p>
       </div>
